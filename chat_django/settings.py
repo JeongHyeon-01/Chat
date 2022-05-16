86ce2ff9ad225441405d5a4a4935e0bbf0a57315
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-05-17 01:00:34 +0900

    [add]chat app _ chapter_01

--- chat_django/settings.py
+++ chat_django/settings.py
@@ -31,13 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
+
+# Channels
+ASGI_APPLICATION = 'chat_django.routing.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
